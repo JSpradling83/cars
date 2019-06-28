4b0cf921ba59faf68c40ca398c5a3f7d863a8b6f
David Spradling <jspradling83@yahoo.com>
2019-06-28 09:06:08 -0500

    Created Car class

---
+++ lib/car.rb
@@ -0,0 +1,9 @@
+class Car
+  attr_reader :make, :model, :color
+
+  def initialize(make:, model:, color:)
+    @make = make
+    @model = model
+    @color = color    
+  end
+end
---
+++ spec/car_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/car'
+
+RSpec.describe Car do
+  describe "#initialize" do
+    it "takes in a make, model, and color" do
+      car = Car.new(make:"Toyota", model:"Camry", color:"Blue")
+      expect(car.make).to eq "Toyota"
+      expect(car.model).to eq "Camry"
+      expect(car.color).to eq "Blue"
+    end
+  end
+end
